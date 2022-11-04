e5d6f6b62c206d18999e3f9a1ce37895e478d9a8
cizar44 <hamerlamateusz@gmail.com>
2022-11-04 19:37:06 +0100

    Add api for user statistics for frontend

--- src/WebUI/clientapp/store/slices/meal/mealSlice.ts
+++ src/WebUI/clientapp/store/slices/meal/mealSlice.ts
@@ -1,31 +1,168 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { Meal, MealTypeEnum } from "../../../model/Meal";
+import { Product } from "../../../model/Product";
+import {
+  DailyUserStatistic,
+  DailyUserStatisticResponse,
+  UserStatistic,
+  UserStatisticResponse,
+} from "../../../model/UserStatistic";
 import { axiosBaseQuery } from "../../services/axiosBaseQuery";
 
 export interface CreateMealCommand {
   name: string;
   type: MealTypeEnum;
+  forDate: string;
+}
+
+export interface CreateMealProductCommand {
+  mealId: string;
+  productId: string;
+  amount: number;
+}
+
+export interface UpdateMealProductCommand {
+  mealId: string;
+  mealProductId: string;
+  amount: number;
+}
+
+export interface GetMealPlansQuery {
+  dateFrom: string;
+  dateTo: string;
+}
+
+export interface GetDailyUserStatisticQuery {
+  date: string;
+}
+
+export interface GetProductListQuery {
+  searchBy?: string | null;
 }
 
 export const mealApi = createApi({
   reducerPath: "meal",
-  tagTypes: ["Meal"],
-  baseQuery: axiosBaseQuery({ baseUrl: "https://localhost:5001/api/" }),
+  tagTypes: ["Meal", "Product"],
+  baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000/api/" }),
   endpoints: (builder) => ({
-    getMeals: builder.query<Meal[], void>({
-      query: () => ({ url: "meal", method: "GET" }),
-      transformResponse: (response: { data: Meal[] }) => response.data,
+    getProducts: builder.query<Product[], GetProductListQuery>({
+      query: () => ({ url: "product", method: "GET" }),
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "Product", id } as const)),
+              { type: "Product", id: "LIST" },
+            ]
+          : [{ type: "Product", id: "LIST" }],
     }),
-    addMeal: builder.mutation<string, CreateMealCommand>({
-      query: (body) => ({
-        url: `meal`,
-        method: "POST",
-        body,
+    getMeals: builder.query<Meal[], void>({
+      query: () => ({ url: "meal/fordate", method: "GET" }),
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "Meal", id } as const)),
+              { type: "Meal", id: "LIST" },
+            ]
+          : [{ type: "Meal", id: "LIST" }],
+    }),
+    getMealPlans: builder.query<UserStatistic[], GetMealPlansQuery>({
+      query: (query) => ({
+        url: `statistic/user?DateFrom=${query.dateFrom}&DateTo=${query.dateTo}`,
+        method: "GET",
       }),
-      invalidatesTags: ["Meal"],
-      transformResponse: (response: { data: string }) => response.data,
+      providesTags: () => [{ type: "Meal", id: "PLAN" }],
+      transformResponse: (response: UserStatisticResponse[]) => {
+        return response.map((c) => ({
+          userId: c.userId,
+          forDate: c.forDate,
+          caloricInfo: {
+            kcal: c.kcal,
+            fats: c.fats,
+            proteins: c.proteins,
+            carbohydrates: c.carbohydrates,
+          },
+        }));
+      },
+    }),
+    getDailyMealPlan: builder.query<
+      DailyUserStatistic,
+      GetDailyUserStatisticQuery
+    >({
+      query: (query) => ({
+        url: `statistic/details?Date=${query.date}`,
+        method: "GET",
+      }),
+      providesTags: () => [{ type: "Meal", id: "PLAN" }],
+      transformResponse: (response: DailyUserStatisticResponse) => {
+        const result: DailyUserStatistic = {
+          userId: response.userId,
+          forDate: response.forDate,
+          caloricInfo: {
+            kcal: response.kcal,
+            fats: response.fats,
+            proteins: response.proteins,
+            carbohydrates: response.carbohydrates,
+          },
+          meals: response.meals.map((c) => ({
+            id: c.id,
+            caloricInfo: {
+              kcal: c.kcal,
+              fats: c.fats,
+              proteins: c.proteins,
+              carbohydrates: c.carbohydrates,
+            },
+            products: c.products.map((x) => ({
+              id: x.id,
+              mealId: x.mealId,
+              productId: x.productId,
+              name: x.name,
+              amount: x.amount,
+              kcal: x.kcal,
+              fats: x.fats,
+              proteins: x.proteins,
+              carbohydrates: x.carbohydrates,
+            })),
+          })),
+        };
+        return result;
+      },
+    }),
+    addMeal: builder.mutation<string, Partial<CreateMealCommand>>({
+      query(data) {
+        return { url: `meal`, method: "POST", data };
+      },
+      invalidatesTags: [{ type: "Meal", id: "PLAN" }],
+    }),
+    addMealProduct: builder.mutation<string, Partial<CreateMealProductCommand>>(
+      {
+        query(data) {
+          return { url: `meal/${data.mealId}/product`, method: "POST", data };
+        },
+        invalidatesTags: [{ type: "Meal", id: "PLAN" }],
+      }
+    ),
+    updateMealProduct: builder.mutation<
+      void,
+      Partial<UpdateMealProductCommand>
+    >({
+      query(data) {
+        return {
+          url: `meal/${data.mealId}/product/${data.mealProductId}`,
+          method: "PUT",
+          data,
+        };
+      },
+      invalidatesTags: [{ type: "Meal", id: "PLAN" }],
     }),
   }),
 });
 
-export const { useAddMealMutation, useGetMealsQuery } = mealApi;
+export const {
+  useAddMealMutation,
+  useGetMealsQuery,
+  useGetMealPlansQuery,
+  useGetDailyMealPlanQuery,
+  useAddMealProductMutation,
+  useGetProductsQuery,
+  useUpdateMealProductMutation,
+} = mealApi;
--- src/WebUI/clientapp/store/store.ts
+++ src/WebUI/clientapp/store/store.ts
@@ -1,10 +1,14 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import layoutReducer from "./slices/layout/layoutSlice";
+import { mealApi } from "./slices/meal/mealSlice";
 
 export const store = configureStore({
   reducer: {
     layout: layoutReducer,
+    [mealApi.reducerPath]: mealApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(mealApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
